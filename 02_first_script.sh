0f385d7aa531388e50b5e9bb60c95afb8ccff1b6
zaira-26 <zaira.hasan656@gmail.com>
2026-08-02 13:00:22 +0100

    Create 02_first_script.sh

---
+++ 02_first_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gene="TP53"
+gene2="BRCA1"
+gene3="EGFR"
+echo "Hello!"
+echo "Welcome to Bash scripting."
+echo "Lets have our first session."
+
+echo "The first gene in the file is:"
+echo $gene
+echo "The second gene in the file is:"
+echo $gene2
+echo "The third gene in the file is:"
+echo $gene3
+
+echo "Enter a gene name:"
+read gene4
+
+echo "You entered:"
+echo $gene4
